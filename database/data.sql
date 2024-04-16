74bc101417661b7817339e47d2c838c923f2e2fd
Connie Chin <conniec96@gmail.com>
2024-04-16 19:03:14 +0000

    Initial commit

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,9 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+insert into "users" ("username", "hashedPassword")
+    values ('coconutHead', 'rfseirfu');
+
+insert into "reviews" ("title", "review", "userId", "rating", "photoUrl")
+      values ('Pixel Perfect', 'a great tech movie that is way ahead of its time', '1', '4', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_6mp_gpdbpSjw8HtuGG2d7IHv5MjH82HNQQ&s');
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,20 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+CREATE TABLE "public"."users" (
+  "userId" serial PRIMARY KEY,
+  "username" text not null,
+  "hashedPassword" text not null,
+  "createdAt" timestamptz(6) not null default now()
+);
+CREATE TABLE "public"."reviews" (
+  "reviewId" serial PRIMARY KEY,
+  "title" text not null,
+  "review" text not null,
+  "userId" integer not null,
+  "rating" text not null,
+  "photoUrl" text not null,
+  "createdAt" timestamptz(6) not null default now(),
+  "updatedAt" timestamptz(6)
+);
+ALTER TABLE "public"."reviews" ADD FOREIGN KEY ("userId") REFERENCES "public"."users" ("userId");
