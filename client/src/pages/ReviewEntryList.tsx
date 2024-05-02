182cac3ba64275129001076f8fe5ba03473155aa
Connie Chin <conniec96@gmail.com>
2024-05-02 18:53:50 +0000

    Async watch list.

--- client/src/pages/ReviewEntryList.tsx
+++ client/src/pages/ReviewEntryList.tsx
@@ -12,7 +12,6 @@ export function ReviewEntryList() {
     async function load() {
       try {
         const reviewsList = await readReviews();
-        console.log('reviewlist', reviewsList);
         setReviewsList(reviewsList);
       } catch (err) {
         setError(err);
